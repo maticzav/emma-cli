c1d0486c6f51ed85b2b86e39d7640fa1da97fa8c
maticzav <matic.zavadlal@gmail.com>
2019-10-27 19:44:35 +0100

    fix: lerna config, add start script

---
+++ scripts/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "DB url: $DATABASE_URL"
+export POSTGRESQL_URL="${DATABASE_URL/postgres\:\/\//postgresql://}"
+echo "Fixed URL: $POSTGRESQL_URL"
+
+echo "DEPLOYING... ($PORT)"
+
+./node_modules/.bin/probot run ./server/dist/index.js -p $PORT
