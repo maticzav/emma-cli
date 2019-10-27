565764a20040ec8936f2cdc8456f581555b29b9a
maticzav <matic.zavadlal@gmail.com>
2019-10-27 14:59:27 +0100

    migration script fix

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+echo "DB url: $DATABASE_URL"
+export POSTGRESQL_URL="${DATABASE_URL/postgres\:\/\//postgresql://}"
+echo "Fixed URL: $POSTGRESQL_URL"
+
 cd server
 yarn prisma:migrate
