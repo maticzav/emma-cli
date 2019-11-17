e95e180348fca467dc0311c7f5a6ff25a0961cf9
maticzav <matic.zavadlal@gmail.com>
2019-11-17 19:48:51 +0100

    fix: photon postgresql url ssl fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "DB url: $DATABASE_URL"
-export POSTGRESQL_URL="${DATABASE_URL/postgres\:\/\//postgresql://}"
+export POSTGRESQL_URL="${DATABASE_URL/postgres\:\/\//postgresql://}?sslaccept=accept_invalid_certs"
 echo "Fixed URL: $POSTGRESQL_URL"
 
 echo "DEPLOYING... ($PORT)"
