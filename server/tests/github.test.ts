4b1362687738167f3ebbe7db92f8e9a15313ab6a
Leza Lutonda (lemol) <lemol-c@hotmail.com>
2019-12-28 22:58:24 +0100

    complete tests for github.ts and utils.ts

--- server/tests/github.test.ts
+++ server/tests/github.test.ts
@@ -1,16 +1,17 @@
-import { resetBranch } from '../src/github'
+import { resetBranch, getRepo, fetchRepos } from '../src/github'
 import { Octokit } from 'probot'
 
 describe('github:', () => {
+  let github: Octokit
+  const owner = 'maticzav'
+  const repo = 'emma-cli'
+
   describe('resetBranch:', () => {
-    let github: Octokit
     let getBranch: jest.Mock
     let deleteRef: jest.Mock
     let getRef: jest.Mock
     let createRef: jest.Mock
 
-    const owner = 'maticzav'
-    const repo = 'emma-cli'
     const branchName = 'test-branch'
     const defaultBranchName = 'default-branch'
 
@@ -91,6 +92,75 @@ describe('github:', () => {
         ref: `refs/heads/${branchName}`,
         sha: 'sha',
       })
+    })
+  })
+
+  describe('fetchRepos', () => {
+    let githubReposGet: jest.Mock
+
+    beforeEach(() => {
+      github = new Octokit()
+      githubReposGet = jest.fn()
+      github.repos.get = githubReposGet as any
+    })
+
+    test('should get multiple repositories', async () => {
+      githubReposGet.mockImplementation(({ owner, repo }) =>
+        Promise.resolve(
+          ({
+            'maticzav/repo1': { data: { name: 'repo1' } },
+            'maticzav/repo2': { data: { name: 'repo2' } },
+            'maticzav/repo3': { data: { name: 'repo3' } },
+          } as any)[`${owner}/${repo}`],
+        ),
+      )
+
+      const result = await fetchRepos(github, [
+        { full_name: 'maticzav/repo1' },
+        { full_name: 'maticzav/repo2' },
+        { full_name: 'maticzav/repo3' },
+      ] as any)
+
+      expect(result).toHaveLength(3)
+      expect(result).toContainEqual({ name: 'repo1' })
+      expect(result).toContainEqual({ name: 'repo2' })
+      expect(result).toContainEqual({ name: 'repo3' })
+    })
+  })
+
+  describe('getRepo', () => {
+    let githubReposGet: jest.Mock
+
+    beforeEach(() => {
+      github = new Octokit()
+      githubReposGet = jest.fn()
+      github.repos.get = githubReposGet as any
+    })
+
+    test('handle correctly if not found', async () => {
+      expect.assertions(2)
+      githubReposGet.mockRejectedValueOnce(new Error('not found'))
+
+      try {
+        await getRepo(github, owner, repo)
+      } catch (error) {
+        expect(error).toEqual(new Error('not found'))
+      }
+
+      expect(githubReposGet).toBeCalledTimes(1)
+    })
+
+    test('should get a single repository', async () => {
+      githubReposGet.mockResolvedValueOnce({
+        data: {
+          name: repo,
+        },
+      })
+
+      const result = await getRepo(github, owner, repo)
+
+      expect(result).toEqual({ name: repo })
+      expect(githubReposGet).toBeCalledTimes(1)
     })
   })
 })
--- server/tests/utils.test.ts
+++ server/tests/utils.test.ts
@@ -1,6 +1,25 @@
-import { withDefault } from '../src/utils'
+import { withDefault, base64, sha } from '../src/utils'
 
-test('withDefault', () => {
-  expect(withDefault(4, undefined)).toBe(4)
-  expect(withDefault(4, 6)).toBe(6)
+describe('utils:', () => {
+  test('withDefault:', () => {
+    expect(withDefault(4, undefined)).toBe(4)
+    expect(withDefault(4, 6)).toBe(6)
+  })
+
+  test('sha', () => {
+    expect(sha('qwerty')).toBe('b1b3773a05c0ed0176787a4f1574ff0075f7521e')
+    expect(sha('1029384756')).toBe('1142b33e04e1bef9f8724b824c54b08899f572a7')
+    expect(sha('asdfg_123')).toBe('49bc7a03bdeb7cc62a516673cb3c2c0267d42aae')
+  })
+
+  describe('base64', () => {
+    test('inverseBase64(base64(x)) === x', () => {
+      const identity = (x: string) =>
+        Buffer.from(base64(x), 'base64').toString()
+
+      expect(identity('qwerty')).toBe('qwerty')
+      expect(identity('1029384756')).toBe('1029384756')
+      expect(identity('asdfg_123')).toBe('asdfg_123')
+    })
+  })
 })
